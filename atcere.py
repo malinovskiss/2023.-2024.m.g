fc59b35a3f5ed98c175bc75cdc2534da21461c80
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-09-04 15:18:39 +0300

    Add files via upload

---
+++ atcere.py
@@ -0,0 +1,23 @@
+#1.uzdevums
+
+print(125+24)
+print(125-24)
+print(125*24)
+print(125/24)
+
+#2.uzdevums
+
+m=int* 0.0254
+
+print(m)
+
+
+#3.uzdevums
+
+baze=float("ievadi bazi")
+augstums=float("ievadi augstumu")
+Laukums= 1/2*baze*augstums
+
+print(augstums)
+
+#4.uzdevums
