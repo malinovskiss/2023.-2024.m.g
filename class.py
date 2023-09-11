68a13f75dd1fb9b821a74b1b0e413917cee4c375
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-09-11 15:03:50 +0300

    Add files via upload

---
+++ class.py
@@ -0,0 +1,6 @@
+class masinas():
+    def __init__(self, bakasTilpums, pasazieruSkaits, motoraTilpums, motoraJauda):
+        self.bakasTilpums = bakasTilpums
+        self.pasazieruSkaits = pasazieruSkaits
+        self.motoraTilpums = motoraTilpums
+        self.motoraJauda = motoraJauda
