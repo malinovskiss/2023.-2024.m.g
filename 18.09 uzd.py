4169bc1147a378d85a8bbcd5c95f6626064cf95b
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-09-18 15:58:59 +0300

    Update 18.09 uzd.py

--- 18.09 uzd.py
+++ 18.09 uzd.py
@@ -1,6 +1,7 @@
-class property():
-    def __init__ (self, Produkta_kategorija, Produkta_nosaukums, Tehniskie_raksturojumi, Nomas_cena_dienā, Produkts_pieejams, Nomnieks_vārds, Nomnieks_uzvārds, Nomnieks_p, Nomnieks_tel_numurs, Nomas_sākuma_datums, Nomas_beigu_datums ):
+import PySimpleGUI as ps
 
+class noma():
+    def __init__(self, Produkta_kategorija, Produkta_nosaukums, Tehniskie_raksturojumi, Nomas_cena_dienā, Produkts_pieejams, Nomnieks_vārds, Nomnieks_uzvārds, Nomnieks_pk,Nomnieks_tel_numurs, Nomas_sākuma_datums, Nomas_beigu_datums):
         self.Produkta_kategorija = Produkta_kategorija
         self.Produkta_nosaukums = Produkta_nosaukums
         self.Tehniskie_raksturojumi = Tehniskie_raksturojumi
@@ -8,7 +9,9 @@ class property():
         self.Produkts_pieejams = Produkts_pieejams
         self.Nomnieks_vārds = Nomnieks_vārds
         self.Nomnieks_uzvārds = Nomnieks_uzvārds
-        self.Nomnieks_p = Nomnieks_p
+        self.Nomnieks_pk = Nomnieks_pk
         self.Nomnieks_tel_numurs = Nomnieks_tel_numurs
         self.Nomas_sākuma_datums = Nomas_sākuma_datums
         self.Nomas_beigu_datums = Nomas_beigu_datums
+        
+    def produktu_info(self):
