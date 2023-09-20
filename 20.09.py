029aad10c979338b9662d532748b6c08284d7b78
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-09-20 15:34:02 +0300

    Update 20.09.py

--- 20.09.py
+++ 20.09.py
@@ -1,27 +1,60 @@
-import PySimpleGUI as ps
+import PySimpleGUI as psg
 
 class noma():
-    def __init__(self, Produkta_kategorija, Produkta_nosaukums, Tehniskie_raksturojumi, Nomas_cena_dienā, Produkts_pieejams, Nomnieks_vārds, Nomnieks_uzvārds, Nomnieks_pk,Nomnieks_tel_numurs, Nomas_sākuma_datums, Nomas_beigu_datums):
+    def __init__(self, Produkta_kategorija, Produkta_nosaukums, Tehniskie_raksturojumi, Nomas_cena_diena, Produkts_pieejams, Nomnieks_vards, Nomnieks_uzvards, Nomnieks_pk,Nomnieks_tel_numurs, Nomas_sakuma_datums, Nomas_beigu_datums):
         self.Produkta_kategorija = Produkta_kategorija
         self.Produkta_nosaukums = Produkta_nosaukums
         self.Tehniskie_raksturojumi = Tehniskie_raksturojumi
-        self.Nomas_cena_dienā = Nomas_cena_dienā
+        self.Nomas_cena_diena = Nomas_cena_diena
         self.Produkts_pieejams = Produkts_pieejams
-        self.Nomnieks_vārds = Nomnieks_vārds
-        self.Nomnieks_uzvārds = Nomnieks_uzvārds
+        self.Nomnieks_vards = Nomnieks_vards
+        self.Nomnieks_uzvards = Nomnieks_uzvards
         self.Nomnieks_pk = Nomnieks_pk
         self.Nomnieks_tel_numurs = Nomnieks_tel_numurs
-        self.Nomas_sākuma_datums = Nomas_sākuma_datums
+        self.Nomas_sakuma_datums = Nomas_sakuma_datums
         self.Nomas_beigu_datums = Nomas_beigu_datums
         
     def produktu_info(self):
           pass
 
-ps.theme('dark')
+psg.theme('darkamber')
 layout = [
-    [ps.Text('A')]
+    [psg.Text('a')]
 ]
 
-window = ps.Window('Produktu pieejams', layout)
+window = psg.Window('Produkts_pieejams', layout)
+
+layout = [
+            [psg.Text('Komponentes')],
+            [psg.Text('Veids'),psg.InputText()],    
+            [psg.Text('Modelis'),psg.InputText()],
+            [psg.Text('Cena'),psg.InputText()],
+            [psg.Button('Saglabāt')]
+            
+          ]
+
+layout2 = [
+            [psg.Text('Redigēšana')], 
+            [psg.Text('Veids'),psg.InputText()],    
+            [psg.Text('Modelis'),psg.InputText()],
+            [psg.Text('Cena'),psg.InputText()],
+            [psg.Button('Saglabāt')]
+        
+        ]
+
+loguGrupa = [[
+    psg.TabGroup(
+        [
+            [
+                psg.Tab('Produkta info', layout),
+                psg.Tab('Nomnieka info', layout2)
+            
+            ]
+        ]
+    ),
+[psg.Button('Aizvērt'),
+    psg.Button('Datu apskate')]
+
+]]
 
 window.close() 
