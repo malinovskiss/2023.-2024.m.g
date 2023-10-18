9b829a582edc1f860a02d4bd0187079d7889f685
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-10-18 15:35:07 +0300

    Update 16.10.py

--- 16.10.py
+++ 16.10.py
@@ -2,3 +2,12 @@ import mysql.connector
 
 db = mysql.connector.connect(host= 'localhost',database='biblioteka',user='root',password='Igars000')
 print(db)
+
+cursor= db.cursor()
+
+sql = ("""
+insert.into biblioteka (lasitajs, gramatas, autors)
+values (%s,%s,%s,%s);
+""")
+dati = (564,"Ģramatas nosaukums")
+db.commit
