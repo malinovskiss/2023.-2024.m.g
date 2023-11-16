48da9bfbe2d85a3a156718545954469e8a8d87ed
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-11-16 15:24:33 +0200

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -5,11 +5,21 @@ URL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 atbilde = requests.get(URL)
 
-print (atbilde)
+# print (atbilde)
 
 dati = atbilde.json()
-
-print(json.dumps(dati,indent = 2))
-
-for a in dati:
-    print(a["USD"])
+#print(dati)
+print(json.dumps(dati,indent = 4))
+#vvsk = {"sakumskola":
+#           {"1":{
+#               'a':{"vards":"Pēteris","uzvards":"Bērziņš"},
+#               'b':{"vards":"Pēteris","uzvards":"Bērziņš"}},
+#             "2":},
+#       "pamatskola":}
+USD = dati['bpi']['USD']
+print(USD)
+USD = dati['bpi']['USD']['rate_float'] #Dabujam bitkoinu cenu USD valuta 
+print(USD)
+ievade = input("Ievadi bitkoinu daudzumu: ")
+matematiskaDarbiba = USD * float(ievade)
+print(f"{ievade} bitkoinu maksa {matematiskaDarbiba}$")
