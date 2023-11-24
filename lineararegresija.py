ab7294749e510ef8124da239155e9601069629a5
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-11-24 12:01:32 +0200

    Update lineararegresija.py

--- lineararegresija.py
+++ lineararegresija.py
@@ -3,18 +3,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = os.getcwd() + '\API\ex1data1.txt'
-data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
-print(data.head())
-print(data.describe())
-data.plot(kind="scatter", x="Population",y="Profit",figsize= (12,8),c = 1)
+path = os.getcwd() + '\ex1data1.txt'
+data = pd.read_csv(path, header=None, names=['Population','Profit'])
+#print(data.head())
+#print(data.describe())
+
+#atelosim datus grafiski
+data.plot(kind="scatter", x="Population", y="Profit", figsize=(12,8), c= 1)
 plt.show()
 
-def computeCost(x,y,theta):
-    inner = np.power(((x* theta.T)-y), 2)
-    return np.sum(inner)/(2*len(x))
+
+#funkcija kļudas apreikināšana1
+def computeCost(X,y,theta):
+    inner= np.power(((X* theta.T)-y), 2)
+    return np.sum(inner)/(2*len(X))
+
+#japievieno dati papildus kolonna
+data.insert(0, 'Ones',1)
+
+#definēt X (termina dati) un y (merka vertība)
+cols = data.shape[1]
+X = data.iloc[:, 0:cols-1]
+y= data.iloc[:, cols-1:cols]
 
 
+#paerveidojam datus matrica
+
+X = np.matrix(X.values)
+y = np.matrix(y.values)
+theta = np.matrix(np.array([0,0]))
+
+print(computeCost(X,y,theta))
 
 def gradientDescent(X, y, theta, alpha, iters):
     temp = np.matrix(np.zeros(theta.shape))
@@ -36,12 +55,22 @@ def gradientDescent(X, y, theta, alpha, iters):
 alpha = 0.01
 iters = 1000
 
-g, cost = gradientDescent(x,y,theta,alpha,iters)
+g, cost = gradientDescent(X,y,theta,alpha,iters)
 print(g)
 
-print(computeCost(x,y,g))
+print(computeCost(X,y,g))
 
-x = np.linspace(data.Population.win(),data.Population.max(), 100)
 
-f = g[0,0] + (g[0,1] * x)
+x=np.linspace(data.Population.min(),data.Population.max(), 100)
 
+f= g[0,0] + (g[0,1]*x)
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(x,f,'r', lable = "Prediction")
+ax.scatter(data.Population, data.profit, lable = "Training data")
+ax.legend(loc=2)
+ax.set_xlable("Population")
+ax.set_ylable("Profit")
+ax.set_title("Preditc profti vs population size")
+
+plt.show()
