91f63055b8340ac2fad7c15357635baa58f27266
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2023-11-29 15:32:49 +0200

    Update datu_strukturas.py

--- datu_strukturas.py
+++ datu_strukturas.py
@@ -1,2 +1,24 @@
 from collections import deque
+
 rinda = deque()
+
+rinda.append(['A','B','C'])
+print("Rinda: ", rinda)
+
+rinda.append("D")
+
+print(rinda)
+
+rinda.appendleft('a')
+print(rinda)
+
+rinda.pop()
+print(rinda)
+
+rinda.popleft()
+print(rinda)
+
+kortezs = ('Jan','feb','mar')
+print(type(kortezs))
+
+print()
